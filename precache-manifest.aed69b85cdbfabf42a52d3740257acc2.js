85c9ae78f8a94b8363c94493c426d57cc92f635b
xdemocle <xdemocle@gmail.com>
2020-08-10 01:02:30 +0200

    ad files

---
+++ precache-manifest.aed69b85cdbfabf42a52d3740257acc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c088119d79baecb78cbc4b903ec6228",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb92bd1078ee43357c7",
+    "url": "/static/css/main.0dc45e62.chunk.css"
+  },
+  {
+    "revision": "a20d309e93414da7660f",
+    "url": "/static/js/2.173640fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.173640fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb92bd1078ee43357c7",
+    "url": "/static/js/main.382fab6d.chunk.js"
+  },
+  {
+    "revision": "da5169963086991031bd",
+    "url": "/static/js/runtime-main.7cfaa34f.js"
+  }
+]);
